f50b8a6edfdff073c862c1f1d2d40d4d26c7f977
Ange <ange.capone@ynov.com>
2025-11-20 10:48:30 +0100

    Update note.js

--- routes/note.js
+++ routes/note.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.get ("/", functio (request, response){
+router.get ("/", function (request, response){
     response.render("note", {})
 })
 
